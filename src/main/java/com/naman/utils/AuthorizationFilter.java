1ac6ba3f34b5512122bc851bf055682e2447c55a
nagoel <nagoel@ITEM-S135840.emea.msad.sopra>
2022-01-25 17:50:33 +0530

    missed changes

--- src/main/java/com/naman/utils/AuthorizationFilter.java
+++ src/main/java/com/naman/utils/AuthorizationFilter.java
@@ -13,7 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebFilter(filterName= "AuthFilter", urlPatterns = {"*.xhtml", "*.jsf"})
+//@WebFilter(filterName= "AuthFilter", urlPatterns = {"*.xhtml", "*.jsf"})
 public class AuthorizationFilter implements Filter{
 	
 	public AuthorizationFilter() {
